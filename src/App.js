d254a7b5773f946643f45168989496137682c1f7
yuji-nakamoto <yuji0411@icloud.com>
2020-03-18 04:13:12 +0900

    change Welcome message to Hello World!

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <img src={logo} className="App-logo" alt="logo" />                     
+	<p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello World!
         </a>
       </header>
     </div>
